55b51740a3cfe1ba51530949a7df2643f70521f6
GitHub eso21309 <GitHub eso21309@gmail.com>
2024-12-06 11:59:21 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_market_app/ui/pages/welcome/welcome_page.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() {
+  runApp(ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: WelcomePage(),
+    );
+  }
+}
